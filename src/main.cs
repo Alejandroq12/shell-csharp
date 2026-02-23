3157bff760072144063e9e776a85a9cbfd8499b9
DamarisCaballero <113944199+DamarisCaballero@users.noreply.github.com>
2026-02-23 07:28:11 -0600

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -7,7 +7,7 @@ class Program
           Console.Write("$ ");
           string? input = Console.ReadLine();
           Console.WriteLine($"{input}: command not found");
-          Console.Write("$ ");
+          Console.WriteLine("$ ");
           Console.Write("$ exit");
           Environment.Exit(0);
         }
