b95f5b51c36ed3fe2d82ab27a3b9876a5d7e9b7f
Alejandroq12 <qjuliodev@gmail.com>
2026-02-25 07:48:22 -0600

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -6,16 +6,19 @@ class Program
         {
           Console.Write("$ ");
           string? input = Console.ReadLine();
+          string[] inputToArray = input.Split(" ");
+
           if (input == "exit")
           {
             Environment.Exit(0);
-          }
-
-          if (input == "echo")
+          } else if (inputToArray[0] == "echo") 
           {
-            Console.WriteLine(input);
+            string stringToPrint = String.Join(" ", inputToArray[1..]);
+            Console.WriteLine(stringToPrint);
+          } else 
+          {
+            Console.WriteLine($"{input}: command not found");
           }
-          Console.WriteLine($"{input}: command not found");
         }
     }
 }
