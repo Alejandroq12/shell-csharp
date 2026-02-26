838aafdd7ae113246226ca8fa2a9684b72435341
Alejandroq12 <qjuliodev@gmail.com>
2026-02-26 07:41:32 -0600

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -15,7 +15,25 @@ class Program
           {
             string stringToPrint = String.Join(" ", inputToArray[1..]);
             Console.WriteLine(stringToPrint);
-          } else 
+          } else if (inputToArray[0] == "type" && inputToArray.Length > 1) 
+          {
+            switch (inputToArray[1])
+            {
+              case "echo":
+                Console.WriteLine($"{inputToArray[1]} is a shell builtin");
+                break;
+              case "exit":
+                Console.WriteLine($"{inputToArray[1]} is a shell builtin");
+                break;
+              case "type":
+                Console.WriteLine($"{inputToArray[1]} is a shell builtin");
+                break;
+              default:
+                Console.WriteLine($"{inputToArray[1]}: not found");
+                break;
+            }
+          }
+          else 
           {
             Console.WriteLine($"{input}: command not found");
           }
